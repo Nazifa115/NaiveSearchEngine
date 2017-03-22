7b2a8ec179fc8eaee794ed0a03326a464780ee8e
nazifakarima <nazifakarima@gmail.com>
2017-03-22 16:13:56 -0400

    Fixed bug with selecting dataset

--- src/Controller/ActionListeners.java
+++ src/Controller/ActionListeners.java
@@ -12,7 +12,6 @@ import java.nio.file.Paths;
 import javax.swing.JTextArea;
 
 import Model.Constants;
-import View.SearchEngineUI;
 
 public class ActionListeners {
 	public static class BuildIndexButtonListener implements ActionListener {
@@ -38,36 +37,33 @@ public class ActionListeners {
 				System.out.println(indexFilesDirectory + " does not exist. Creating Directory...");
 				try {
 					File tempFile = new File(indexFilesDirectory + Constants.DATASET);
-					// CONSTANTS.INDEXPATH =
-					// file.getParentFile().getCanonicalPath() + "/Indexes";
-
 					System.out.println(tempFile.getParentFile().getCanonicalPath() + " created.");
 				} catch (IOException e) {
 					System.out.println(e);
 					e.printStackTrace();
 				}
-
 			}
-
 		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			createIndexFolderIfNonExisting();
 
-			if (Constants.INPUTFILESDIRECTORY != null && Constants.DATASET != null) {
-				if (Constants.DATASET.equalsIgnoreCase("Cranfield")) {
-					new Indexer(Constants.INPUTFILESDIRECTORY + Constants.DATASET, Constants.INDEXFILEDIRECTORY);
+			if (Constants.INPUTFILESDIRECTORY != null) {
+				if (Constants.DATASET == null) {
+					new Indexer(Constants.INPUTFILESDIRECTORY + Constants.Cranfield_DATASET, Constants.INDEXFILEDIRECTORY);
+				}else  if (Constants.DATASET.equalsIgnoreCase("Cranfield")) {
+					new Indexer(Constants.INPUTFILESDIRECTORY, Constants.INDEXFILEDIRECTORY);
 				}
-
 			}
 		}
-
 	}
 
 	public static class SearchButtonActionListener implements ActionListener {
 		String queryText;
 		JTextArea resultArea;
+		BufferedWriter writer = null;
+		
 		public SearchButtonActionListener(String qText, JTextArea rTArea) {
 			this.queryText = qText;
 			this.resultArea = rTArea;
@@ -83,7 +79,22 @@ public class ActionListeners {
 		}
 
 		private void search(String queryText2) {
-			
+			writer = initWriter();
+		}
+
+		private BufferedWriter initWriter() {
+			String path = Constants.OUTPUTFILESDIRECTORY+ Constants.DATASET + "_results.txt";
+			try {
+				if (Files.exists(Paths.get(path))) {
+					writer = new BufferedWriter(new FileWriter(path, true));
+				} else {
+					File f = new File(path);
+					writer = new BufferedWriter(new FileWriter(f.getCanonicalPath(), true));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return writer;
 		}
 	}
 }
--- src/Model/Constants.java
+++ src/Model/Constants.java
@@ -3,7 +3,9 @@ package Model;
 public class Constants {
 
 	public static String ROOTDIR = "/Users/nazifakarima/Documents/Nazifa/WrightState/Spring17/CS7800/Assignment1/";
-	public static String DATASET = "Cranfield";
+	public static String DATASET;
+	public static String Cranfield_DATASET = "Cranfield";
+	public static String Medline_DATASET = "Medline";
 	public static String INDEXFILEDIRECTORY = ROOTDIR + "Indices/";
 	public static String INPUTFILESDIRECTORY = ROOTDIR;
 	public static String OUTPUTFILESDIRECTORY = ROOTDIR + "Output/";
